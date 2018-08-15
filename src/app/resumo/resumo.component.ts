67ad84a323e864d116c27a2cced3b3af2a72805e
dancesar <danillo.yamagush@gmail.com>
2018-08-15 00:58:10 -0300

    OnDestroy garantir a destruicao dos componentes em conexoes lentas

--- src/app/resumo/resumo.component.ts
+++ src/app/resumo/resumo.component.ts
@@ -1,21 +1,25 @@
 import { ResumoService } from './resumo.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-resumo',
   templateUrl: './resumo.component.html',
   styleUrls: ['./resumo.component.css']
 })
-export class ResumoComponent implements OnInit {
+export class ResumoComponent implements OnInit, OnDestroy {
 
   resumo;
+  inscricao;
 
   constructor(private resumoService:ResumoService) {
 
    }
 
   ngOnInit() {
-    this.resumoService.getResumo().subscribe(dados => this.resumo = dados);
+    this.inscricao = this.resumoService.getResumo().subscribe(dados => this.resumo = dados);
   }
 
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
 }
